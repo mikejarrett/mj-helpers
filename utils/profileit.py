10431723af4189a1924f8ee131a2676cdae130f5
Mike Jarrett <michael.jarrett@polestarglobal.com>
2015-11-27 08:26:04 +0000

    Initial commit of profileit decorator

---
+++ utils/profileit.py
@@ -0,0 +1,32 @@
+# -*- coding: utf8 -*-
+"""
+A decorator to profile function calls.
+
+Usage:
+    from profileit import profileit
+
+    @profileit
+    def foo():
+        return do_stuff()
+
+In a Python shell:
+    >>> form pstats import Stats
+    >>> stats = Stats('/tmp/foo.profile')
+    >>> stats.sort_stats('cumulative').print_stats(50)
+
+"""
+
+
+import cProfile
+
+def profileit(func):
+
+    def wrapper(*args, **kwargs):
+        datafn = '/tmp/{0}.profile'.format(func.__name__)
+        prof = cProfile.Profile()
+        retval = prof.runcall(func, *args, **kwargs)
+        prof.dump_stats(datafn)
+
+        return retval
+
+    return wrapper
